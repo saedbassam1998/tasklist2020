24b5cf17535a08a0696968de1d118a0fd59166aa
saedkhali <saaedkhalil1998@gmail.com>
2020-03-05 11:01:28 +0200

    saed2

--- database/migrations/2020_03_05_083041_create_tasks_table.php
+++ database/migrations/2020_03_05_083041_create_tasks_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreatePostsTable extends Migration
+class CreateTasksTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,10 +13,9 @@ class CreatePostsTable extends Migration
      */
     public function up()
     {
-        Schema::create('posts', function (Blueprint $table) {
+        Schema::create('tasks', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string ('title');
-            $table->mediumText('body');
+            $table->string('title');
             $table->timestamps();
         });
     }
@@ -28,6 +27,6 @@ class CreatePostsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('posts');
+        Schema::dropIfExists('tasks');
     }
 }
