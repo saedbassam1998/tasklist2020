e0f0b7824f7000841ab5ff1fc3c69f82eb481192
saedkhali <saaedkhalil1998@gmail.com>
2020-03-14 10:47:17 +0200

    saaed mod

--- database/migrations/2020_03_14_083914_create_tasks_table.php
+++ database/migrations/2020_03_14_083914_create_tasks_table.php
@@ -15,7 +15,7 @@ class CreateTasksTable extends Migration
     {
         Schema::create('tasks', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('title');
+            $table->string('name');
             $table->timestamps();
         });
     }
